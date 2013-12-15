4e227db4fd284a4ddc743f137951d59f7325fbe1
siad007 <siad.ardroumli@gmail.com>
2013-12-15 16:25:23 +0100

    Add first working state

--- src/siad007/Fop/Command.php
+++ src/siad007/Fop/Command.php
@@ -4,12 +4,19 @@ namespace siad007\Fop;
 
 class Fop extends Options
 {
+    /**
+     * @var array
+     */
     protected $args = array(
+        'fo'  => '',
         'xml' => '',
         'xsl' => '',
-        'pdf' => ''
+        'pdf' => 'document.pdf'
     );
 
+    /**
+     * @param string $args
+     */
     public function __construct($args = null)
     {
         if ($args !== null) {
@@ -17,15 +24,46 @@ class Fop extends Options
         }
     }
 
+    /**
+     * @throws \InvalidArgumentException
+     */
     public function generatePdf()
     {
-        $cmd = sprintf(
-            'fop -xml %s -xls %s -pdf %s',
-            $this->args['xml'],
-            $this->args['xsl'],
-            $this->args['pdf']
-        );
+        if ($this->hasArgumentFo()) {
+            $cmd = sprintf(
+                'fop -fo %s -pdf %s',
+                $this->args['fo'],
+                $this->args['pdf']
+            );
+        } elseif ($this->hasXmlInputFiles()) {
+            $cmd = sprintf(
+                'fop -xml %s -xls %s -pdf %s',
+                $this->args['xml'],
+                $this->args['xsl'],
+                $this->args['pdf']
+            );
+        } else {
+            throw new \InvalidArgumentException(
+                'Please specify a fo file or xml/xsl files.'
+            );
+        }
 
         exec(escapeshellcmd($cmd));
+    }
+
+    /**
+     * @return boolean
+     */
+    protected function hasArgumentFo()
+    {
+        return !empty($this->args['fo']);
+    }
+
+    /**
+     * @return boolean
+     */
+    protected function hasXmlInputFiles()
+    {
+        return !empty($this->args['xml']) && !empty($this->args['xsl']);
     }
 }
