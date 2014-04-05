49fc69522eb8b31d88e2ed504d4007b8cfab8bd3
siad007 <siad.ardroumli@gmail.com>
2014-04-05 17:17:36 +0200

    Fixed code style issues and added comments

--- src/siad007/Fop/Command.php
+++ src/siad007/Fop/Command.php
@@ -5,21 +5,27 @@
  * PHP Version 5.3
  *
  * @copyright Siad Ardroumli
+ * @author siad.ardroumli@gmail.com
  * @license http://www.opensource.org/licenses/mit-license.php MIT
  * @link https://github.com/siad007/Fop
  */
 
 namespace siad007\Fop;
 
+/**
+ * FOP Command Facade.
+ */
 class Command
 {
     /**
+     * FOP CLI Arguments.
      *
      * @var Arguments
      */
     protected $args = null;
 
     /**
+     * FOP CLI Options.
      *
      * @var Options
      */
@@ -28,8 +34,8 @@ class Command
     /**
      * Standard constructor.
      *
-     * @param string $args
-     * @param string $opts
+     * @param Arguments $args
+     * @param Options $opts
      *            optional: default null
      */
     public function __construct(Arguments $args, Options $opts = null)
@@ -43,16 +49,33 @@ class Command
         }
     }
 
+    /**
+     * Generates the PDF.
+     *
+     * @return void
+     */
     public function generatePdf()
     {
         exec(escapeshellcmd((string) $this));
     }
 
+    /**
+     * Get Arguments.
+     *
+     * @return Arguments
+     */
     public function getArguments()
     {
         return $this->args;
     }
 
+    /**
+     * Set Arguments.
+     *
+     * @param Arguments $args
+     *
+     * @return Command
+     */
     public function setArguments(Arguments $args)
     {
         $this->args = $args;
@@ -60,11 +83,23 @@ class Command
         return $this;
     }
 
+    /**
+     * Get Options.
+     *
+     * @return Options
+     */
     public function getOptions()
     {
         return $this->opts;
     }
 
+    /**
+     * Set Options.
+     *
+     * @param Options $opts
+     *
+     * @return Command
+     */
     public function setOptions(Options $opts)
     {
         $this->opts = $opts;
@@ -73,6 +108,8 @@ class Command
     }
 
     /**
+     * String representation of the FOP cli command.
+     *
      * @return string
      *
      * @throws \InvalidArgumentException
@@ -80,24 +117,14 @@ class Command
     public function __toString()
     {
         if ($this->args->hasArgumentFo()) {
-            $cmd = sprintf(
-                'fop %s -fo %s -pdf %s',
-                (string) $this->opts,
-                $this->args['fo'],
-                $this->args['pdf']
-            );
+            $cmd = sprintf('fop %s -fo %s -pdf %s', (string) $this->opts, $this->args['fo'], $this->args['pdf']);
         } elseif ($this->args->hasXmlInputFiles()) {
-            $cmd = sprintf(
-                'fop %s -xml %s -xls %s -pdf %s',
-                (string) $this->opts,
-                $this->args['xml'],
-                $this->args['xsl'],
-                $this->args['pdf']
-            );
+            $xml = $this->args['xml'];
+            $xsl = $this->args['xsl'];
+            $pdf = $this->args['pdf'];
+            $cmd = sprintf('fop %s -xml %s -xls %s -pdf %s', (string) $this->opts, $xml, $xsl, $pdf);
         } else {
-            throw new \InvalidArgumentException(
-                'Please specify a fo file or xml/xsl files.'
-            );
+            throw new \InvalidArgumentException('Please specify a fo file or xml/xsl files.');
         }
 
         return $cmd;
